ca1f0eb5ece9444ed9aeb2e5b5214a8c24130fcd
Ricardo Robles <rroblesgo@gmail.com>
2017-11-22 21:37:01 +0100

    Añadidos configuraciones webpack-dev-server y extraccion css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 let config = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, './public'),
         filename: 'output.js'
     },
     module: {
@@ -13,10 +16,23 @@ let config = {
             },
             {
                 test: /\.scss$/,
-                loader: ['style-loader', 'css-loader', 'sass-loader']
+                use: ExtractTextWebpackPlugin.extract({ // call our plugin with extract method
+                    use: ['css-loader', 'sass-loader'], // Use these loaders
+                    fallback: 'style-loader' // fallback for any CSS not extracted
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin('styles.css') // call plugin constructor with name our css file
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, './public'), // A directory or url to serve html content
+        historyApiFallback: true, //fallback to /index.html for Single Page Apps
+        inline: true, // inline mode (set to falsle to disable including client scripts)
+        open: true // open default browser while launching
+    },
+    devtool: 'eval-source-map' // enable devtool for better debugging experience
 }
 
 module.exports = config;
